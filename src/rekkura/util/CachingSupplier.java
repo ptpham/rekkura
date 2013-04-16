09aee6a421827d7d6adbca737beb4e365e659fcc
Peter Pham <silentmagesoul@gmail.com>
2013-04-15 20:06:04 -0700

    Adding a comment (cop-out for today).

--- src/rekkura/util/CachingSupplier.java
+++ src/rekkura/util/CachingSupplier.java
@@ -6,6 +6,13 @@ import java.util.Set;
 import com.google.common.base.Supplier;
 import com.google.common.collect.Sets;
 
+/**
+ * This supplier retains a record of the objects that it creates
+ * for future reference or collection by some other data structure.
+ * @author ptpham
+ *
+ * @param <T>
+ */
 public abstract class CachingSupplier<T> implements Supplier<T> {
 	public final Set<T> created = Sets.newHashSet();
 	
