1a1279a26365cc624902d1b9a756289cf962850b
ptpham <ptpham@cs.stanford.edu>
2013-05-25 19:12:19 -0700

    Augmenting cache with invalidation.

--- src/rekkura/util/Cache.java
+++ src/rekkura/util/Cache.java
@@ -14,20 +14,25 @@ public class Cache<U, V> {
 
 	public final Map<U, V> stored = Maps.newHashMap();
 	private final Function<U, V> fn;
+	private final Function<V, Boolean> checker;
 	
-	protected Cache(Function<U, V> fn) { this.fn = fn; }
+	protected Cache(Function<U, V> fn) { this(fn, null); }
+	protected Cache(Function<U, V> fn, Function<V, Boolean> checker) {
+		this.checker = checker;
+		this.fn = fn;
+	}
 	
 	public V get(U u) {
-		V result = stored.get(u);
+		V result = validate(stored.get(u));
 		if (result == null) {
 			result = fn.apply(u);
 			stored.put(u, result);
 		}
 		return result;
 	}
-	
+
 	public V propose(U u, V v) {
-		V result = stored.get(u);
+		V result = validate(stored.get(u));
 		if (result == null) {
 			stored.put(u, v);
 			return v;
@@ -35,8 +40,18 @@ public class Cache<U, V> {
 		return result;
 	}
 	
+	private V validate(V v) {
+		if (v == null || checker == null || checker.apply(v)) return v;
+		return null;
+	}
+	
 	public static <U, V> Cache<U, V> create(Function<U, V> fn) {
 		return new Cache<U, V>(fn);
+	}
+	
+	public static <U, V> Cache<U, V> create(Function<U, V> fn,
+		Function<V, Boolean> checker) {
+		return new Cache<U, V>(fn, checker);
 	}
 	
 	public String toString() {
