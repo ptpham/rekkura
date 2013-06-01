5b5e02b35fe86262843391623b9a910b5f726ac8
Peter Pham <silentmagesoul@gmail.com>
2013-05-31 20:13:23 -0700

    Getting rid of a warning.

--- src/rekkura/ggp/milleu/Match.java
+++ src/rekkura/ggp/milleu/Match.java
@@ -2,7 +2,6 @@ package rekkura.ggp.milleu;
 
 import java.util.List;
 
-import com.google.common.eventbus.EventBus;
 import rekkura.ggp.milleu.Game.Config;
 import rekkura.ggp.player.RemotePlayer;
 import rekkura.util.Colut;
