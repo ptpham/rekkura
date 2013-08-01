06819cc855355ac831657f340987688f932ee2a4
ptpham <silentmagesoul@gmail.com>
2013-07-31 19:32:54 -0700

    Getting rid of deprecation warnings.

--- test/rekkura/test/logic/TopperTest.java
+++ test/rekkura/test/logic/TopperTest.java
@@ -6,8 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 import rekkura.state.algorithm.Topper;
